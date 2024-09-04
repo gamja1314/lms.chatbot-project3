f69ebd0bc7a875ed1966b4d0e8e464f93c9ac7af
SungHunKKKKK <wsaxd22@gmail.com>
2024-09-04 12:53:07 +0900

    quiz, answer test

---
+++ lms/src/test/java/com/test/lms/ShTest.java
@@ -0,0 +1,25 @@
+package com.test.lms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.test.lms.entity.Quiz;
+import com.test.lms.entity.QuizAnswer;
+
+@SpringBootTest
+public class ShTest {
+
+    @Test
+    public static void test(String[] args){
+
+        Quiz q = new Quiz();
+        q.setQuiz("현장맞춤 AI 자바 스프링부트 담당 강사 이름은?");
+        q.setAnswer("최상배 강사님");
+
+        QuizAnswer qa = new QuizAnswer();
+        qa.setQuiz(q);
+        qa.setAnswerSave("최상배 강사님");
+
+        System.out.println(qa.showQuizAnswer());
+    }
+}
