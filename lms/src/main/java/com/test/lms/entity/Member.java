c8ea0b3e626051d54db47d0295ee0f134e711ef9
gamja1314 <gamja941104@gmail.com>
2024-09-05 15:46:10 +0900

    회원가입, Security 설정

--- lms/src/main/java/com/test/lms/entity/Member.java
+++ lms/src/main/java/com/test/lms/entity/Member.java
@@ -16,26 +16,29 @@ import lombok.Setter;
 public class Member {
     
     
-    @Id
+    
+    @Id 
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long memberNum;
+    private Long memberNum; // 회원번호
 
-    @Column(unique = true)
-    private String username;
+    @Column(unique = true, length = 30)
+    private String username;    // 아이디
 
-    @Column(nullable = false)
-    private String password;
+    @Column(nullable = false, length = 50)
+    private String password;    // 비밀번호
 
-    @Column(unique = true)
-    private String nickname;
+    @Column(unique = true, length = 30)
+    private String nickname;    // 닉네임
 
-    @Column(nullable = false)
-    private String email;
+    @Column(nullable = false, length = 30)
+    private String email;   // 이메일
 
-    private String rank;
+    @Column(length = 20)
+    private String userRank;    // 랭크
 
-    private String role;
+    @Column(length = 20)
+    private String role;    // 권한
 
-    private LocalDateTime createTime;
+    private LocalDateTime createTime;   // 가입날짜
 
 }
--- lms/src/main/java/com/test/lms/service/MemberService.java
+++ lms/src/main/java/com/test/lms/service/MemberService.java
@@ -1,5 +1,9 @@
 package com.test.lms.service;
 
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.test.lms.entity.Member;
@@ -12,20 +16,38 @@ import lombok.RequiredArgsConstructor;
 public class MemberService {
 
     private final MemberRepository memberRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
 
-    // member 가입
+    // 회원 가입
     public Member create(String username, String password, String nickname, String email) {
         
+        if (memberRepository.findByUsername(username).isPresent()) {
+            throw new IllegalArgumentException("이미 사용 중인 아이디입니다.");
+        }
+
+        if (memberRepository.findByUsername(email).isPresent()) {
+            throw new IllegalArgumentException("이미 사용 중인 이메일입니다.");
+        }
+
         Member member = new Member();
 
         member.setUsername(username);
-        member.setPassword(password);
+        member.setPassword(passwordEncoder.encode(password));
         member.setNickname(nickname);
         member.setEmail(email);
+        member.setCreateTime(LocalDateTime.now());
 
         this.memberRepository.save(member);
-
         return member;
     }
 
+    // 로그인 
+    public Optional<Member> login(String username, String password) {
+        Optional<Member> memberOpt = memberRepository.findByUsername(username);
+        if (memberOpt.isPresent() && passwordEncoder.matches(password, memberOpt.get().getPassword())) {
+            return memberOpt;
+        } else {
+            return Optional.empty();
+        }
+    }
 }
