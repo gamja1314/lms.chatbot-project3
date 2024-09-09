e4962cf174ad8a9746014848b0b6b24454bd0d55
SungHunKKKKK <wsaxd22@gmail.com>
2024-09-09 13:50:41 +0900

    RestQuizAnswerController
    를 만들었음

--- lms/src/main/java/com/test/lms/controller/QuizController.java
+++ lms/src/main/java/com/test/lms/controller/QuizController.java
@@ -1,12 +1,10 @@
 package com.test.lms.controller;
 
 import org.springframework.data.domain.Page;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -47,15 +45,15 @@ public class QuizController {
     }
 
     //퀴즈 정답 제출 및 검증
-    @PostMapping("/submit")
-    public ResponseEntity<Boolean> submitQuiz(@RequestParam Long quizId, @RequestParam String answer, @RequestParam boolean isPublic, @RequestParam String userName){ 
+    // @PostMapping("/submit")
+    // public ResponseEntity<Boolean> submitQuiz(@RequestParam Long quizId, @RequestParam String answer, @RequestParam boolean isPublic, @RequestParam String userName){ 
         
-        Boolean isCorrect = quizService.submitQuizAnswer(quizId, answer, isPublic, userName);
+    //     Boolean isCorrect = quizService.submitQuizAnswer(quizId, answer, isPublic, userName);
 
-        return ResponseEntity.ok(isCorrect);
-        //제출 시 정답 공개 여부 설정
-        // quizService.submitAnswer(quizId, correct, isPublic, Id);
-        // return ResponseEntity.ok("정답 제출 완료");
-    }
+    //     return ResponseEntity.ok(isCorrect);
+    //     //제출 시 정답 공개 여부 설정
+    //     // quizService.submitAnswer(quizId, correct, isPublic, Id);
+    //     // return ResponseEntity.ok("정답 제출 완료");
+    // }
 
 }
