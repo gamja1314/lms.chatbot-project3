01642d0aec8f058364365ce28a8ad0234d1a6468
MinSeokKun <kimms51567115@gmail.com>
2024-09-11 02:19:21 +0900

    챌린지 등록 수정 조회 및 마크다운 에디터 변경

--- react-lms/src/AppRoutes.js
+++ react-lms/src/AppRoutes.js
@@ -6,13 +6,16 @@ import SignUp from './pages/SignUp';
 import CodingTest from './pages/CodingTest';
 import CodingPage from './pages/CodingTestPage';
 import MyPage from './pages/MyPage';
-import Admin from './pages/Admin/Admin';
-import AdminQuiz from './pages/Admin/AdminQuiz';
-import QuizForm from './pages/Admin/QuizForm';
+import Admin from './components/Admin/Admin';
+import AdminQuiz from './components/Admin/AdminQuiz';
+import QuizForm from './components/Admin/QuizForm';
 import SolvedQuiz from './pages/SolvedQuiz';
+import ChallengeAdmin from './components/Admin/AdminChallenge';
 import ProtectedRoute from './components/ProtectedRoute';
+import ChallengePage from './pages/ChallengeDetail';
 import { useAuth } from './AuthContext';
 import ChangePassword from './pages/ChangePassword';
+import ChallengeForm from './components/Admin/ChallengeForm';
 
 const AppRoutes = () => {
   const { username } = useAuth();
@@ -29,8 +32,12 @@ const AppRoutes = () => {
       <Route path='/admin' element={<ProtectedRoute><Admin /></ProtectedRoute>} />
       <Route path='/admin/quiz' element={<ProtectedRoute><AdminQuiz /></ProtectedRoute>} />
       <Route path='/admin/quiz/edit' element={<QuizForm />}></Route>
+      <Route path='/admin/challenges' element={<ProtectedRoute><ChallengeAdmin /></ProtectedRoute>} />
+      <Route path='/admin/challenges/edit' element={<ProtectedRoute><ChallengeForm /></ProtectedRoute>} />
+      <Route path='/admin/challenges/edit/:id' element={<ProtectedRoute><ChallengeForm /></ProtectedRoute>} />
       <Route path='/admin/quiz/edit/:quizId' element={<QuizForm />}></Route>
       <Route path="/change-password" element={<ChangePassword />} />
+      <Route path='/challenges/:id' element={<ChallengePage />}/>
     </Routes>
   ), [username]);  // username이 변경될 때만 재생성
 
