539093eb84800fc3446dc5778becd66d795af5b7
gamja1314 <gamja941104@gmail.com>
2024-09-20 17:46:44 +0900

    게시판 작업중...

---
+++ lms/src/main/java/com/test/lms/repository/BoardRepository.java
@@ -0,0 +1,19 @@
+package com.test.lms.repository;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.test.lms.entity.Board;
+
+public interface BoardRepository extends JpaRepository<Board, Long> {
+
+    //페이징 처리
+    Page<Board> findAll(Pageable pageable);
+
+    @Query("SELECT n FROM Board n ORDER BY n.createDate DESC")
+    List<Board> findRecent5(Pageable pageable);
+}
